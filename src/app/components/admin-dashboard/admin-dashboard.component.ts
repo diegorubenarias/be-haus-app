f7e45bee7d8a1ec8224ee15604ba38ef38c2c10a
diegorubenarias <diego.arias@gmail.com>
2025-08-11 20:36:37 +0000

    add calendar

--- src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -14,6 +14,7 @@ import { Router, RouterOutlet } from '@angular/router';
 })
 export class AdminDashboardComponent implements AfterViewInit, OnInit {
   user: any = {};
+  isSidebarCollapsed = true;
   ngOnInit(): void {
     this.user = JSON.parse(localStorage.getItem('user') || '{}');
   }
--- src/app/components/calendario/calendario.component.ts
+++ src/app/components/calendario/calendario.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { RoomService } from '../../services/room.service';
 
 @Component({
   selector: 'app-calendario',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './calendario.component.html',
   styleUrl: './calendario.component.scss'
 })
-export class CalendarioComponent {
+export class CalendarioComponent implements OnInit {
+  
+  months = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+  selectedMonthIndex = 1;
+  rooms: any;
+  daysInMonth: number[] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+  roomService = inject(RoomService);
+   ngOnInit(): void {
+    this.roomService.getRooms().subscribe((data: any) => {
+        this.rooms = data.sort((a: any, b: any) => {
+          if (a.building === b.building) {
+            return a.roomNumber - b.roomNumber;
+          }
+          return a.building.localeCompare(b.building);
+        });
 
+      });
+  }
+  selectMonth(month: number) {
+
+  }
 }
